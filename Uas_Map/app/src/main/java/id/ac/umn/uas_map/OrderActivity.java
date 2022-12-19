c72071edaec1cb7f58f2b34dd919f9b591c54100
henrylaufrans <henry.laufrans@student.umn.ac.id>
2022-12-19 10:10:53 +0700

    tambahin kamera

--- Uas_Map/app/src/main/java/id/ac/umn/uas_map/OrderActivity.java
+++ Uas_Map/app/src/main/java/id/ac/umn/uas_map/OrderActivity.java
@@ -1,7 +1,9 @@
 package id.ac.umn.uas_map;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
@@ -10,8 +12,10 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputLayout;
@@ -28,7 +32,9 @@ public class OrderActivity extends AppCompatActivity {
     String[] items =  {"1","2","3","4"};
     AutoCompleteTextView autoCompleteTxt;
     ArrayAdapter<String> adapterItems;
-
+    private static final int REQUEST_CODE = 22;
+    Button btnpicture;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -93,6 +99,17 @@ public class OrderActivity extends AppCompatActivity {
                 startActivity(Time);
             }
         });
+
+        btnpicture = findViewById(R.id.btncamera_id);
+        imageView = findViewById(R.id.imageView1);
+
+        btnpicture.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, REQUEST_CODE);
+            }
+        });
     }
 
     boolean isPhone(EditText text) {
@@ -115,5 +132,17 @@ public class OrderActivity extends AppCompatActivity {
             Phone.setError("Phone number is required!");
         }
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }else{
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+        }
+
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
